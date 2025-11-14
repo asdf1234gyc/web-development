e000c18facca325a500be7849257fb86e3744249
asdf1234gyc <1483875459@qq.com>
2025-11-14 19:32:58 +0800

    final version

---
+++ WebApplication1/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        
+       public int UniversityCampusId { get; set; }
+
+        
+        public virtual UniversityCampus UniversityCampus { get; set; }
+    }
+}
